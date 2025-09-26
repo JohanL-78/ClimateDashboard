81e0a0859d5ca0eafa5408c3243e8d81830ba7b4
Johan Lorck <kfj.lorck@orange.fr>
2025-09-26 15:49:12 +0200

    fetch

--- app/data/page.js
+++ app/data/page.js
@@ -3,7 +3,7 @@ import DataPageContent from '@/components/DataPageContent';
 
 // ISR: revalide toutes les 24 heures pour sync avec les MAJ de NASA
 // et éviter les mismatch avec le cache de la route
-export const revalidate = 86400; // 24 heures
+export const revalidate = 86400; // 24 hours
 
 export default async function DataPage() {
   const data = await fetchNasaDataServer();
